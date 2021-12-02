5cfaac39437df791455e648b803a5af806ae2057
jerem <jeremy.lardenois@hotmail.fr>
2021-12-02 18:59:34 +0100

    change linux rights

--- encode.py
+++ encode.py
@@ -1,12 +1,14 @@
 import json
 
-data = {"foo": "bar"}
+# data = "pornhub.com"
 
-text_to_encode = json.dumps(data)
+#text_to_encode = json.dumps(data)
+text_to_encode = "pornhub.com"
+
 
 text_encoded = "" 
 
 for char in text_to_encode:
     text_encoded += "%" + hex(ord(char))[2:]
 
-print (text_encoded)
+print ("https://" + text_encoded)
