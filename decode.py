5af8441c09a0faab8d1373418d1c46f7ce40a72a
JeremyLARDENOIS <jeremy.lardenois@etu.umontpellier.fr>
2022-11-02 10:59:04 +0100

    feat: Make a better decoder

--- decode.py
+++ decode.py
@@ -1,7 +1,22 @@
-text_encoded = input("Text encoded: ").split("%")[1:]
-text_decoded = ""
+#!/usr/bin/python3
 
-for char in text_encoded:
-    text_decoded += chr(int(char,16))
+'''Allow to decode encoded string'''
 
-print (text_decoded)
+
+def main():
+    """main function"""
+    text_encoded = input("Text encoded: ")
+    text_decoded = ""
+    i = 0
+    while i < len(text_encoded):
+        if text_encoded[i] == '%':
+            text_decoded += chr(int(text_encoded[i+1:i+3], 16))
+            i += 3
+        else:
+            text_decoded += text_encoded[i]
+            i += 1
+
+    print(text_decoded)
+
+
+main()
